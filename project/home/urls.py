ae8ea9175a4a29859238e116c1182ed5f46a827f
nicolasm96 <nicomeza96@gmail.com>
2023-10-05 14:08:52 -0300

    Mejora visual

--- project/home/urls.py
+++ project/home/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from . import views
 
@@ -7,3 +8,5 @@ urlpatterns = [
 path("", views.index, name="index"),
 path("about/", views.about, name="about"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
